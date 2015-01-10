fc617a34b8b8b6060437d1ad49c7a466f603423b
suprbh <supb27@yahoo.com>
2015-01-09 17:42:42 -0800

    small commit

--- src/dancer_1.js
+++ src/dancer_1.js
@@ -1,33 +1,33 @@
 var TapDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this,top,left,timeBetweenSteps);
-  this.top = top;
-  this.left = left;
-  this.oldStep = Dancer.prototype.step;
-  this.timeBetweenSteps = timeBetweenSteps;
+  //this.oldStep = Dancer.prototype.step;
   //debugger;
-  this.$node=$('<span onmouseover="mouseEventHandler(this);" class="tapDancer"></span>');
+  this.$node=$('<span class="tapDancer"></span>');
   this.setPosition(top,left);
-  this.step();
-  //debugger;
-  // this.image = new Image();
-  // this.image.src='/Users/HR10/Desktop/2014-09-subclass-dance-party/images/thumbnail.gif';
 };
 
 TapDancer.prototype = Object.create(Dancer.prototype);
 TapDancer.prototype.construct = TapDancer;
 
-TapDancer.prototype.setPosition = function(top, left){
-  //debugger;
-  var styleSettings = {
-      top: top,
-      left: left
-    };
-  this.$node.css(styleSettings);
-};
+// TapDancer.prototype.setPosition = function(top, left){
+//   var styleSettings = {
+//       top: top,
+//       left: left
+//     };
+//   this.$node.css(styleSettings);
+// };
 TapDancer.prototype.step = function(){
+  Dancer.prototype.step.call(this);
+
+  //this.$node.toggle();
   //debugger;
-  //debugger;
-  this.oldStep();
-  //debugger;
-  this.$node.toggle();
+  //var that = this;
+  /*setInterval(function() {
+    debugger;
+    that.$node.animate({ left: $('body').width()}, 3000, 'linear', function() {
+      $(that.$node).css({ left: - $(that.$node).width()});
+    });
+  }, 10);*/
+  
 };
+
